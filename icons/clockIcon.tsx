7e2cff5aca6f955993af22a4dcaa875913cdac4e
Rick1196 <ricardompp11@gmail.com>
2022-12-29 19:22:29 -0600

    first commit

---
+++ icons/clockIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ClockIcon: React.FC = () => {
+  return (
+    <svg
+      width="29"
+      height="40"
+      viewBox="0 0 29 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M21.115 1.66666H7.88501C0.20834 1.66666 -0.383744 7.86333 3.76084 11.2367L25.2392 28.7633C29.3838 32.1367 28.7917 38.3333 21.115 38.3333H7.88501C0.20834 38.3333 -0.383744 32.1367 3.76084 28.7633L25.2392 11.2367C29.3838 7.86333 28.7917 1.66666 21.115 1.66666Z"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+export default ClockIcon;
---
+++ icons/hamburgerIcon.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const HamburgerMenu: React.FC = () => {
+  return (
+    <svg
+      width="18"
+      height="12"
+      viewBox="0 0 9 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M0 1H9M0 7H9M0 3.82353H9" stroke="black" />
+    </svg>
+  );
+};
+
+export default HamburgerMenu;
