d8350b10fd43e450033a3b335dbb90d664cc1dae
ricardo <ricardompp11@gmail.com>
2023-01-02 21:21:04 -0600

    add experience and fact types

---
+++ types/experience.ts
@@ -0,0 +1,10 @@
+export type Experience = {
+  from: string;
+  to: string;
+  title: string;
+  description: any;
+  companyType: string;
+  position: string;
+  technologies: string[];
+  company: string;
+};
---
+++ types/index.ts
@@ -0,0 +1,3 @@
+import { Experience } from "./experience";
+import { Fact } from "./fact";
+export type { Experience, Fact };
