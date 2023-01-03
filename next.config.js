d92b6d23d17796ec26eed4ca6839f2632a0ca0d3
ricardo <ricardompp11@gmail.com>
2023-01-02 21:22:36 -0600

    add images external domains

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "https://static.licdn.com",
+      "static.licdn.com",
+      "https://github.githubassets.com",
+      "github.githubassets.com",
+      "ssl.gstatic.com"
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
