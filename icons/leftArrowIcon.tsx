6f82465c05f003b85b0189bdfee49e4ff0e5170e
ricardo <ricardompp11@gmail.com>
2023-01-03 22:43:26 -0600

    add new svg icon

---
+++ icons/leftArrowIcon.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const LeftArrowIcon: React.FC = () => {
+  return (
+    <svg
+      width="9"
+      height="18"
+      viewBox="0 0 9 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.08997 1.08001L1.56997 7.60001C0.799965 8.37001 0.799965 9.63001 1.56997 10.4L8.08997 16.92"
+        stroke="black"
+        stroke-width="1.5"
+        stroke-miterlimit="10"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default LeftArrowIcon;
