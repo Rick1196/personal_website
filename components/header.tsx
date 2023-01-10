cfe2a40347cf0d0bd15f3faa3a7ae7910ee967d7
ricardo <ricardompp11@gmail.com>
2023-01-09 22:00:09 -0600

    add articles link

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import HamburgerMenu from "../icons/hamburgerIcon";
@@ -30,6 +31,42 @@ const Header: React.FC<{
           >
             <SonIcon color={darkModeEnabled ? "white" : "black"} />
           </button>
+          <button
+            onClick={() => setDisplayMobileMenu((prev) => !prev)}
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 bg-transparent md:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <HamburgerMenu />
+          </button>
+        </div>
+        <div
+          className={`${
+            displayMobileMenu ? "" : "hidden"
+          } items-center justify-between w-full md:flex md:w-auto md:order-1`}
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 bg-transparent md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0">
+            <li>
+              <Link
+                href="/"
+                className="block py-2 pl-3 pr-4 text-white rounded text-gray-700 md:p-0 dark:text-white"
+              >
+                About me
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/articles"
+                className="block py-2 pl-3 pr-4 text-white rounded text-gray-700 md:p-0 dark:text-white"
+              >
+                My Articles
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
