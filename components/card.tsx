14b1a02b028dd29fca518206702159e60530ac6e
Rick1196 <ricardompp11@gmail.com>
2023-01-11 20:23:55 -0600

    create articles responsive layout

--- components/card.tsx
+++ components/card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Card: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
-    <article className="w-full shadow-card border border-grey-500 rounded p-4 min-h-[150px]">
+    <article className="w-full shadow-card dark:shadow-darkCard border border-grey-500 rounded p-4 min-h-[150px]">
       {children}
     </article>
   );
--- pages/articles.tsx
+++ pages/articles.tsx
@@ -1,27 +1,82 @@
 import { XMLParser } from "fast-xml-parser";
 import { InferGetStaticPropsType } from "next";
-import Image from "next/image";
 import Link from "next/link";
-import parse from "html-react-parser";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/card";
 import { DevFeed } from "../types/feed";
 
 export default function Articles({
   articles,
+  categories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [query, setQuery] = useState<string | null>(null);
+  const [filteredArtilces, setFilteredArticles] = useState(
+    articles.rss.channel.item
+  );
+
+  useEffect(() => {
+    if (query) {
+      setFilteredArticles(
+        articles.rss.channel.item.filter((article) =>
+          article.category?.includes(query)
+        )
+      );
+    }
+  }, [query, articles]);
+
   return (
-    // TODO: make responsive this view
-    <main className="h-view">
+    <div className="flex flex-col span-2">
       <Link href={articles.rss.channel.link} target="_blank">
         <h1 className="text-center w-full mt-6 mb-6 text-2xl dark:text-white">
           {articles.rss.channel.description}
         </h1>
       </Link>
-      <div className="flex flex-row gap-6">
-        <div className="min-w-[30%] max-w-[30%] p-4">Categories</div>
-        <div className="flex flex-col gap-4 min-w-[60%] max-w-[60%]">
-          {articles.rss.channel.item.map((article, index) => (
+      <div className="flex md:flex-row flex-col gap-6">
+        <div className="flex md:flex-col flex-row flex-wrap md:min-w-[30%] md:max-w-[30%] p-4 gap-4">
+          <h1 className="text-center w-full p-2 text-xl dark:text-white">
+            Categories
+          </h1>
+          <div className="flex md:flex-col md:flex-row flex-wrap gap-4">
+            <button
+              onClick={() => {
+                setQuery(null);
+                setFilteredArticles(articles.rss.channel.item);
+              }}
+              key={`clear_filter`}
+              className={`rounded p-2 dark:shadow-darkButton shadow-button dark:text-white border`}
+              aria-label={`clear filter`}
+            >
+              Clear filter
+            </button>
+            {Object.values(categories).map((category) => (
+              <button
+                onClick={() => setQuery(category.tag)}
+                key={`filter_by_${category.tag}`}
+                className={`flex flex-row items-center justify-center gap-2 rounded p-2 dark:shadow-darkButton shadow-button ${
+                  query === category.tag
+                    ? "bg-gray-500 black dark:bg-white border-gray-500"
+                    : "border"
+                }`}
+                aria-label={`filter ${category.tag}`}
+              >
+                <span
+                  className={`w-full text-gray-800 ${
+                    query === category.tag
+                      ? "dark:text-grey-800"
+                      : "dark:text-white"
+                  }`}
+                >
+                  {category.tag}
+                </span>
+                <span className="shadow-bullet bg-blue-100 rounded p-1">
+                  {category.count}
+                </span>
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 md:min-w-[60%] md:max-w-[60%] p-2">
+          {filteredArtilces.map((article, index) => (
             <Card key={article.title}>
               <div className="flex flex-col gap-4">
                 <h1 className="text-left w-full md:text-xl text-l dark:text-white">
@@ -29,13 +84,13 @@ export default function Articles({
                 </h1>
                 <div className="flex flex-row justify-end gap-2">
                   <button
-                    className="border rounded p-2 dark:text-white"
+                    className="border rounded p-2 dark:text-white shadow-button dark:shadow-darkButton"
                     aria-label="read on dev.to"
                   >
                     Read on Dev.to
                   </button>
                   <button
-                    className="border rounded p-2 dark:text-white"
+                    className="border rounded p-2 dark:text-white shadow-button dark:shadow-darkButton"
                     aria-label="read here"
                   >
                     Read here
@@ -46,7 +101,7 @@ export default function Articles({
                     ? article.category.map((tag, tagIndex) => (
                         <span
                           key={`${index}_${tag}_${tagIndex}`}
-                          className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-white dark:text-grey-800"
+                          className="shadow-bullet bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-white dark:text-grey-800"
                         >
                           {tag}
                         </span>
@@ -58,7 +113,7 @@ export default function Articles({
           ))}
         </div>
       </div>
-    </main>
+    </div>
   );
 }
 
@@ -66,10 +121,24 @@ export async function getStaticProps() {
   const xmlData = await (await fetch("https://dev.to/feed/rick1196")).text();
   const parser = new XMLParser();
   const articles: DevFeed = parser.parse(xmlData);
+  const articlesItems = articles.rss.channel.item;
+  const categories: Map<string, { tag: string; count: number }> = new Map();
+  for (const article of articlesItems) {
+    for (const tag of article.category || []) {
+      if (!categories.has(tag)) {
+        categories.set(tag, { tag, count: 1 });
+      } else {
+        const currentValue = categories.get(tag);
+        categories.set(tag, { tag, count: (currentValue?.count || 0) + 1 });
+      }
+    }
+  }
+  console.log(Object.fromEntries(categories));
 
   return {
     props: {
       articles,
+      categories: Object.fromEntries(categories),
     },
     revalidate: 10,
   };
