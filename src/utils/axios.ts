969d4b86cd4752f59c2854eaebdaaf0ff4f97e0e
ricardo <ricardompp11@gmail.com>
2025-11-23 16:23:23 -0600

    fix: env variables

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import { default as baseAxios } from "axios";
-
-const axios = baseAxios.create({ baseURL: 'http://localhost:3000', headers:{
+const apiUrl = import.meta.env.VITE_API_URL;
+const axios = baseAxios.create({ baseURL: apiUrl, headers:{
     'If-None-Match': undefined
 } });
 export default axios;
--- views/github-projects/index.tsx
+++
@@ -1,5 +0,0 @@
-import api from "../../utils/api";
-
-export const preload = () => {
-    void api.getGithubProjects();
-}
